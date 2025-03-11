4ae13560f7a71aedda31b6f3dcc580c972205f24
zevjr <zejunior.py@gmail.com>
2025-03-11 13:08:52 -0300

    refact: service award

--- outsera/razzie_api/app/services/award_service.py
+++ outsera/razzie_api/app/services/award_service.py
@@ -1,27 +1,28 @@
+from collections import defaultdict
 from ..models import Movie
+
+def get_producer_award_years(movies):
+    producers = defaultdict(list)
+    
+    for movie in movies:
+        for producer in map(str.strip, movie.producers.replace('and', ',').split(',')):
+            producers[producer].append(movie.year)
+    
+    return producers
 
 def calculate_award_intervals():
     movies = Movie.query.filter_by(winner=True).order_by(Movie.year).all()
-    producers = {}
-
-    for movie in movies:
-        for producer in movie.producers.replace('and',',').split(','):
-            producer = producer.strip()
-            if producer not in producers:
-                producers[producer] = []
-            producers[producer].append(movie.year)
-
-    intervals = []
-    for producer, years in producers.items():
-        if len(years) > 1:
-            for i in range(1, len(years)):
-                interval = years[i] - years[i - 1]
-                intervals.append(
-                    {
-                        'producer': producer,
-                        'interval': interval,
-                        'previousWin': years[i - 1],
-                        'followingWin': years[i],
-                    }
-                )
+    producers = get_producer_award_years(movies)
+    
+    intervals = [
+        {
+            'producer': producer,
+            'interval': years[i] - years[i - 1],
+            'previousWin': years[i - 1],
+            'followingWin': years[i],
+        }
+        for producer, years in producers.items() if len(years) > 1
+        for i in range(1, len(years))
+    ]
+    
     return intervals
